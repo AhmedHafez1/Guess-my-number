5e4ab729a8cf3ff4507d44cfe51117bd690d2f1e
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2022-12-28 11:09:28 +0200

    added highScore

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
 'use strict';
+
+let secretNumber = generateSecret();
+let score = 20;
+
+const inputElement = document.querySelector('input');
+const scoreElement = document.querySelector('.score');
+const secretElement = document.querySelector('.number');
+const messageElement = document.querySelector('.message');
+const highScoreElement = document.querySelector('.highscore');
+
+document.querySelector('.check').addEventListener('click', checkNumber);
+document.querySelector('.again').addEventListener('click', reset);
+
+function checkNumber() {
+  if (score < 1) return;
+
+  const enteredNumber = Number(inputElement.value);
+
+  if (!isAcceptedNumber(enteredNumber)) {
+    messageElement.textContent = '⛔ أدخل رقم بين 1 و 20';
+    return;
+  }
+
+  if (enteredNumber === secretNumber) {
+    messageElement.textContent = '👍 أحسنــــت';
+    document.body.style.backgroundColor = '#60b347';
+    secretElement.textContent = secretNumber;
+    secretElement.style.width = '30rem';
+    if (score > Number(highScoreElement.textContent)) {
+      highScoreElement.textContent = score;
+    }
+  } else if (enteredNumber > secretNumber) {
+    messageElement.textContent = '📈 أعلى من الرقم السري';
+    score--;
+  } else {
+    messageElement.textContent = '📉 أفل من الرقم السري';
+    score--;
+  }
+
+  if (score < 1) {
+    messageElement.textContent = '💥 لقد خسرت';
+    secretElement.textContent = secretNumber;
+    document.body.style.backgroundColor = '#ef6047';
+  }
+
+  scoreElement.textContent = score;
+}
+
+function isAcceptedNumber(value) {
+  return value && value > 0 && value <= 20;
+}
+
+function reset() {
+  score = 20;
+  scoreElement.textContent = score;
+  messageElement.textContent = 'ابدأ التخمين';
+  inputElement.value = '';
+  secretNumber = generateSecret();
+  document.body.style.backgroundColor = '#222';
+  secretElement.style.width = '15rem';
+  secretElement.textContent = '?';
+}
+
+function generateSecret() {
+  return Math.trunc(Math.random() * 20) + 1;
+}
